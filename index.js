1c10ceeea53b34eed8088eedb579fbd13404bea8
Sushil Dhakal <sushildhakal1@gmail.com>
2021-10-29 15:56:58 +1100

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /**
  * Babel ES6, ES7 to ES5
  */
-require('babel-register');
-require('babel-polyfill');
+require("babel-register");
+require("babel-polyfill");
 
 /**
  * Run file .env to set environment variables
  */
-require('dotenv').config();
-
+require("dotenv").config();
 
 /**
  * Run server with syntax ES6, ES7
  */
-require('./server');
+require("./server");
